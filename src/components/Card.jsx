9c1074b784073990179f610cf2ed4f2abbf2240e
charbel bou ghazale <charbel.boughazale.@gmail.com>
2023-03-04 04:37:57 +0200

    udpated slider

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,18 @@
 import React from 'react';
 import './card.css';
 
-export default function Card({backdrop_path}){
-    //https://image.tmdb.org/t/p/w500//irwQcdjwtjLnaA0iErabab9PrmG.jpg
 
-/* {
-            "adult": false,
-            "backdrop_path": "/irwQcdjwtjLnaA0iErabab9PrmG.jpg",
-            "genre_ids": [
-                28,
-                12,
-                53
-            ],
-            "id": 646389,
-            "original_language": "en",
-            "original_title": "Plane",
-            "overview": "After a heroic job of successfully landing his storm-damaged aircraft in a war zone, a fearless pilot finds himself between the agendas of multiple militias planning to take the plane and its passengers hostage.",
-            "popularity": 2277.913,
-            "poster_path": "/qi9r5xBgcc9KTxlOLjssEbDgO0J.jpg",
-            "release_date": "2023-01-12",
-            "title": "Plane",
-            "video": false,
-            "vote_average": 6.9,
-            "vote_count": 683
-        },*/
+export default function Card({title, popularity, releaseDate, voteAverage, poster}){
 
 return(
     <div className='cardContainer'>
-        <div className="imgContainer">
-            <img src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`} alt="image" />
+        <div className='imgContainer'>
+            <img src={`https://image.tmdb.org/t/p/w500/${poster}`} alt='image' />
         </div>
+        {title} <br />
+        {popularity} <br />
+        {releaseDate} <br />
+        {voteAverage} <br />
     </div>
 )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,13 @@ import {React, useState, useEffect} from 'react';
 import './home.css';
 import Card from '../components/Card';
 import axios from 'axios';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
 
 /*
  * 1 consume the api
- * must have a slider
+ * must have a slider swiper.js
  * tv shows or movies select ratio
  * must have cards
 */
@@ -26,11 +29,29 @@ async function getPopular(){
         console.log(e);
     }       
 }
-console.log(popularData);
+ 
 return (
 <>
-    <h1>this is home</h1>
-    <Card backdrop_path ={popularData[0]?.backdrop_path} />
+
+    <Card releaseDate = {popularData[0]?.release_date} voteAverage = {popularData[0]?.vote_average}
+    title = {popularData[0]?.title} popularity = {popularData[0]?.popularity}
+     poster = {popularData[0]?.poster_path} />
+
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
 </>
 )
 }
