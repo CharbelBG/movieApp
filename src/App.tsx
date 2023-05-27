b5c131afe07f31793bf8b14ccb94f2834a5c269d
charbelBG <charbel.boughazale@gmail.com>
2023-05-27 19:26:13 +0300

    updated navigation

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,16 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Base />} />
-        <Route path='/movie' element={ <h1>Render a selected movie</h1> } />
-
+        <Route path='/' element={<Base />}>
+          <Route path='movies' element={ <h1>Render a selected movie</h1> } />
+          <Route path='topcharts' element={ <h1>Render Charts</h1> } />
+          <Route path='genres' element={ <h1>Render Genres</h1> } />
+          <Route path='newReleases' element={ <h1>Render new releases</h1> } />
+          <Route path='favorites' element={ <h1>Render favorites</h1> } />
+          <Route path='profile' element={ <h1>Render Profile</h1> } />
+          <Route path='info' element={ <h1>Render info</h1> } />
+          <Route path='*' element={ <h1>fallback</h1> } />
+        </Route>
       </Routes>
     </>
   )
